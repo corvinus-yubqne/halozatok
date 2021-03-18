127f3a870e657e14d5f915a9ff5cb64a93924d0f
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-03-18 11:36:53 +0100

    Elkészítettem JS-ben a Pascal-háromszöget.

---
+++ HajosTeszt/gyak2/wwwroot/jsgyak.js
@@ -0,0 +1,26 @@
+﻿window.onload = () => {
+    console.log("Oldal betöltődött")
+
+    var fakt = function (n) {
+        var er = 1;
+        for (var i = 2; i <= n; i++) {
+            er = er * i;
+        }
+        return er;
+    }
+
+    let hova = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        hova.appendChild(sor);
+
+
+        for (var o = 0; o <= s; o++) {
+            let elem = document.createElement("div");
+            elem.classList.add("elem");
+            elem.innerText = fakt(s) / (fakt(o) * fakt(s - o));
+            sor.appendChild(elem);
+        }
+    }
+}
