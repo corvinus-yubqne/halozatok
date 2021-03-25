1ab9bbcca62c57bb24a235fff7cd221ef28fe107
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-03-25 14:35:30 +0100

    4 és fél órás szenvedés után: megjeleníti a kérdéseket és válaszokat, a képeket, a gombok működnek, a válaszok színeződnek. Azonban az üres src-hez nem sikerült az if függvényt megalkotnom, még visszatérek majd rá.

---
+++ HajosTeszt/gyak2/wwwroot/hetedik.js
@@ -0,0 +1,88 @@
+﻿window.onload = letöltés()
+
+var kérdés;
+
+function letöltés() {
+    fetch('questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data));
+
+    function letöltésBefejeződött(d) {
+        console.log("Sikeres letöltés")
+        console.log(d)
+        kérdés = d;
+        kérdésMegjelenítés(0)
+    }
+}
+
+function kérdésMegjelenítés(k) {
+    let ide_kerdes = document.getElementById("kérdés_szöveg")
+    ide_kerdes.innerHTML = kérdés[k].questionText;
+    console.log(`$ {kérdés.length} kérdés érkezett}`)
+
+    for (var i = 1; i < 4; i++) {
+        let elem_kérdés = document.getElementById("válasz" + i)
+        elem_kérdés.innerText = kérdés[k]["answer" + i]
+    }
+
+
+    /*if (document.getElementById("kép2").getAttribute('src') == "") {
+        document.createElement("&nbsp"); 
+    }
+    else { */document.getElementById("kép2").src = "https://szoft1.comeback.hu/hajo/" + kérdés[k].image /*}*/
+
+    document.getElementById("gomb1").onclick = function vissza() {
+        visszaszín();
+
+        if (k == 0) {
+            kérdésMegjelenítés(2);
+        }
+        else {
+            kérdésMegjelenítés(k - 1);         
+        }
+    }
+
+    document.getElementById("gomb2").onclick = function előre() {
+        visszaszín();
+
+        if (k == 2) {
+            kérdésMegjelenítés(0);
+        }
+        else {
+            kérdésMegjelenítés(k + 1);
+        }
+    }
+
+    document.getElementById("válasz1").onclick = function válasz1() {
+        if (kérdés[k].correctAnswer == 1) {
+            document.getElementById("válasz1").style.backgroundColor = "green";
+        }
+        else {
+            document.getElementById("válasz1").style.backgroundColor = "red";
+        }
+    }
+
+    document.getElementById("válasz2").onclick = function válasz2() {
+        if (kérdés[k].correctAnswer == 2) {
+            document.getElementById("válasz2").style.backgroundColor = "green";
+        }
+        else {
+            document.getElementById("válasz2").style.backgroundColor = "red";
+        }
+    }
+
+    document.getElementById("válasz3").onclick = function válasz3() {
+        if (kérdés[k].correctAnswer == 3) {
+            document.getElementById("válasz3").style.backgroundColor = "green";
+        }
+        else {
+            document.getElementById("válasz3").style.backgroundColor = "red";
+        }
+    }
+
+    function visszaszín() {
+        document.getElementById("válasz1").style.backgroundColor = '#8860d0';
+        document.getElementById("válasz2").style.backgroundColor = '#8860d0';
+        document.getElementById("válasz3").style.backgroundColor = '#8860d0';
+    }
+}
