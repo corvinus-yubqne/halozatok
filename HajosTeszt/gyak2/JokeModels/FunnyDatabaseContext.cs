aeaab33d1664ae7c3f0cc8ac721264756ab86b8a
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-05-15 14:37:33 +0200

    Megcsináltam a 12. heti gyakot, kivéve a kérdésmegjelenítést, csak az első kérdést tudtam megjeleníteni, az egész tömböt nem sikerült :( Cserébe egy vicc vár mindenkire aki a jokes.html oldalamra téved.

---
+++ HajosTeszt/gyak2/JokeModels/FunnyDatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace gyak2.JokeModels
+{
+    public partial class FunnyDatabaseContext : DbContext
+    {
+        public FunnyDatabaseContext()
+        {
+        }
+
+        public FunnyDatabaseContext(DbContextOptions<FunnyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Joke> Jokes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=bit.uni-corvinus.hu;Initial Catalog=FunnyDatabase;User ID=vendeg;Password=12345");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Hungarian_CI_AS");
+
+            modelBuilder.Entity<Joke>(entity =>
+            {
+                entity.HasKey(e => e.JokeSk);
+
+                entity.ToTable("Joke");
+
+                entity.Property(e => e.JokeSk).HasColumnName("JokeSK");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
